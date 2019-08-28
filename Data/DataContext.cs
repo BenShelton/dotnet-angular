1587f30e2b6cbe936b6daef167fc3251f2e127f9
BenShelton <bensheltonjones@gmail.com>
2019-08-28 09:45:12 +0100

    Lesson 23

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace DotNet.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
