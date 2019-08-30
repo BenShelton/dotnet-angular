b16ae75a4ab75dd6cee13a95149e678f7bfbc49d
BenShelton <bensheltonjones@gmail.com>
2019-08-30 10:59:21 +0100

    Lesson 41

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+    this.authService.login(this.model)
+      .subscribe(() => {
+        console.log('Logged in successfully');
+      }, () => {
+        console.log('Failed to login');
+      });
   }
 
 }
