40adc92f20d7228b4b87b475e95546652065fc89
BenShelton <bensheltonjones@gmail.com>
2019-08-30 10:09:34 +0100

    Lesson 33

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DotNet.API.Data;
 using DotNet.API.Dtos;
 using DotNet.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DotNet.API.Controllers
 {
@@ -11,29 +17,66 @@ namespace DotNet.API.Controllers
   public class AuthController : ControllerBase
   {
     private readonly IAuthRepository _repo;
-    public AuthController(IAuthRepository repo)
+    private readonly IConfiguration _config;
+    public AuthController(IAuthRepository repo, IConfiguration config)
     {
       _repo = repo;
+      _config = config;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
     {
-        // TODO: validate request
+      // TODO: validate request
 
-        userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+      userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-        if (await _repo.UserExists(userForRegisterDto.Username))
-            return BadRequest("Username already exists");
+      if (await _repo.UserExists(userForRegisterDto.Username))
+        return BadRequest("Username already exists");
 
-        var userToCreate = new User
-        {
-            Username = userForRegisterDto.Username
-        };
+      var userToCreate = new User
+      {
+        Username = userForRegisterDto.Username
+      };
 
-        var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+      var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
-        return StatusCode(201); // TODO: Return user route
+      return StatusCode(201); // TODO: Return user route
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+    {
+      var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+      if (userFromRepo == null)
+        return Unauthorized();
+
+      var claims = new[]
+      {
+        new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+        new Claim(ClaimTypes.Name, userFromRepo.Username)
+      };
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8
+        .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = creds
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+
+      return Ok(new {
+        token = tokenHandler.WriteToken(token)
+      });
     }
   }
 }
