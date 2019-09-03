3b7efe3e399c946b5407e523a3a022411774f16b
BenShelton <bensheltonjones@gmail.com>
2019-09-03 12:33:37 +0100

    Lesson 56

--- DotNet-SPA/src/app/services/auth.service.ts
+++ DotNet-SPA/src/app/services/auth.service.ts
@@ -7,17 +7,27 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   providedIn: 'root'
 })
 export class AuthService {
+  tokenPath = 'token';
   baseUrl = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) { }
+
+  loadToken() {
+    const token = localStorage.getItem(this.tokenPath);
+    if (token) {
+      this.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model)
       .pipe(
         map((response: any) => {
           if (response) {
-            localStorage.setItem('token', response.token);
+            localStorage.setItem(this.tokenPath, response.token);
+            this.decodedToken = this.jwtHelper.decodeToken(response.token);
           }
         })
       );
@@ -28,7 +38,7 @@ export class AuthService {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem(this.tokenPath);
     return !this.jwtHelper.isTokenExpired(token);
   }
 }
