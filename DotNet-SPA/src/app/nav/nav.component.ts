d708ecab40135e92d10785ec8666d3679b0de326
BenShelton <bensheltonjones@gmail.com>
2019-09-03 12:23:16 +0100

    Lesson 55

--- DotNet-SPA/src/app/nav/nav.component.ts
+++ DotNet-SPA/src/app/nav/nav.component.ts
@@ -25,8 +25,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn(): boolean {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout(): void {
