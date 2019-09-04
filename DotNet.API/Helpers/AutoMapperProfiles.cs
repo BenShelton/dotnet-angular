1b8e4cd1959d0bd33e0d0fea8e1375a9ec789621
BenShelton <bensheltonjones@gmail.com>
2019-09-04 11:08:05 +0100

    Lesson 77

--- DotNet.API/Helpers/AutoMapperProfiles.cs
+++ DotNet.API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using DotNet.API.Dtos;
 using DotNet.API.Models;
@@ -8,8 +9,17 @@ namespace DotNet.API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailedDto>();
+            CreateMap<User, UserForListDto>()
+                .ForMember( dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt =>
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember( dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt =>
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
 }
