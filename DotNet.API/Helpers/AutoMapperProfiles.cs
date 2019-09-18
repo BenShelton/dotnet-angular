a84264f0eea346b8bbc0e01fa9a1de68e35ce657
BenShelton <bensheltonjones@gmail.com>
2019-09-18 11:40:19 +0100

    Lesson 99

--- DotNet.API/Helpers/AutoMapperProfiles.cs
+++ DotNet.API/Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,7 @@ namespace DotNet.API.Helpers
                 .ForMember(dest => dest.Age, opt =>
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
