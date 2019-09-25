ea122ccc4834fa7c9230c202b09f253222942f66
BenShelton <bensheltonjones@gmail.com>
2019-09-25 14:07:56 +0100

    Lesson 129

--- DotNet.API/Helpers/AutoMapperProfiles.cs
+++ DotNet.API/Helpers/AutoMapperProfiles.cs
@@ -23,6 +23,7 @@ namespace DotNet.API.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
