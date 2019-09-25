353bc5febecf723c0d70969d20e7404bccca4b95
BenShelton <bensheltonjones@gmail.com>
2019-09-25 11:16:51 +0100

    Lesson 119

--- DotNet-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DotNet-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -70,4 +70,16 @@ export class PhotoEditorComponent implements OnInit {
         this.alertify.error(error);
       });
   }
+
+  deletePhoto(photo: Photo) {
+    this.alertify.confirm('Are you sure you want to delete this photo?', () => {
+      this.userService.deletePhoto(this.authService.decodedToken.nameid, photo.id)
+        .subscribe(() => {
+          this.photos.splice(this.photos.findIndex(p => p === photo), 1);
+          this.alertify.success('Photo has been deleted');
+        }, () => {
+          this.alertify.error('Failed to delete the photo');
+        });
+    });
+  }
 }
