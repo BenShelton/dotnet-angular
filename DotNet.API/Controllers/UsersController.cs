d1da127445ee363bfa7a24619309de391180f5b0
BenShelton <bensheltonjones@gmail.com>
2019-09-27 12:53:28 +0100

    Lesson 135

--- DotNet.API/Controllers/UsersController.cs
+++ DotNet.API/Controllers/UsersController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DotNet.API.Data;
 using DotNet.API.Dtos;
+using DotNet.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet.API.Controllers
 {
+  [ServiceFilter(typeof(LogUserActivity))]
   [Authorize]
   [Route("api/[controller]")]
   [ApiController]
