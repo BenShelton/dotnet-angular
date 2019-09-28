0b9eeee6ac6bca15ac265b5ade7c7c4f157afe7b
BenShelton <bensheltonjones@gmail.com>
2019-09-28 08:32:51 +0100

    Lesson 142

--- DotNet-SPA/src/app/members/member-list/member-list.component.ts
+++ DotNet-SPA/src/app/members/member-list/member-list.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 import { AlertifyService } from '../../services/alertify.service';
+import { Pagination, PaginatedResult } from 'src/app/models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -12,6 +13,7 @@ import { AlertifyService } from '../../services/alertify.service';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(private route: ActivatedRoute, private userService: UserService, private alertify: AlertifyService) { }
 
@@ -19,7 +21,23 @@ export class MemberListComponent implements OnInit {
     this.route.data.subscribe(
       data => {
         this.users = data.users.result;
+        this.pagination = data.users.pagination;
       }
     );
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe((res: PaginatedResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+      }, error => {
+        this.alertify.error(error);
+      })
   }
 }
