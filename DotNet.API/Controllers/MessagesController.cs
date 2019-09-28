e8f4f4a03454ce14b6e7842b3368f576a96c6f8b
BenShelton <bensheltonjones@gmail.com>
2019-09-28 11:46:31 +0100

    Lesson 165

--- DotNet.API/Controllers/MessagesController.cs
+++ DotNet.API/Controllers/MessagesController.cs
@@ -14,7 +14,7 @@ namespace DotNet.API.Controllers
 {
   [ServiceFilter(typeof(LogUserActivity))]
   [Authorize]
-  [Route("api/users/{userId}[controller]")]
+  [Route("api/users/{userId}/[controller]")]
   [ApiController]
   public class MessagesController : ControllerBase
   {
