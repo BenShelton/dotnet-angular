4a378e1e3c56152bbec45ee5888a9d14e54d9b50
BenShelton <bensheltonjones@gmail.com>
2019-09-28 08:24:42 +0100

    Lesson 141

--- DotNet-SPA/src/app/services/user.service.ts
+++ DotNet-SPA/src/app/services/user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
+import { PaginatedResult } from '../models/pagination';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,22 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl);
+  getUsers(page: number, itemsPerPage: number): Observable<PaginatedResult<User[]>> {
+    const paginatedResult = new PaginatedResult<User[]>();
+    const params = new HttpParams()
+      .append('pageNumber', String(page))
+      .append('pageSize', String(itemsPerPage));
+    return this.http.get<User[]>(this.baseUrl, { observe: 'response', params })
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          const paginatedHeaders = response.headers.get('Pagination')
+          if (paginatedHeaders) {
+            paginatedResult.pagination = JSON.parse(paginatedHeaders);
+          }
+          return paginatedResult;
+        })
+      );
   }
 
   getUser(id: number): Observable<User> {
